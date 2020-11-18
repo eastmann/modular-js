aa143cca0a5a6cccb735a332ae27516e93ed882c
Maxim Klochkov <m.klochkov@sap.com>
2020-11-18 19:06:19 +0300

    Converting spaghetti code into Object Literal Module

---
+++ README.md
@@ -0,0 +1,19 @@
+# Modular JS
+
+## Some ground rules:
+
+- Self-contained module
+  - No global variable
+  - Everything to do with my module is in my module
+  - If a module manages more than one thing, it should be split up
+- Separation of concerns
+- DRY code
+- Efficient DOM usage
+  - Very few $(selections)
+- No memory leaks
+  - All events can be unbound
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,27 +1,68 @@
-console.log('[JS] : Script loaded');
+// console.log('[JS] : Script loaded');
 
-$(function() {
-    console.log('[jQuery] : Document ready');
+(function() {
 
-    var people = [];
-    var template = $('#people-template').html();
+    var people = {
+        people: ['John', 'George'],
+        cacheDom() {
+            this.$el = $('#peopleModule');
+            this.$button = this.$el.find('button');
+            this.$input = this.$el.find('input');
+            this.$ul = this.$el.find('ul');
+            this.template = this.$el.find('#people-template').html();
+        },
+        bindEvents() {
+            this.$button.on('click', this.addPerson.bind(this));
+            this.$ul.on('click', 'i.del', this.deletePerson.bind(this));
+        },
+        addPerson() {
+            this.people.push(this.$input.val());
+            this.render();
+            this.$input.val('');
+        },
+        deletePerson(event) {
+            var $remove =$(event.target).closest('li');
+            var i = this.$ul.find('li').index($remove);
 
-    $('#peopleModule').find('button').on('click', function() {
-        people.push($('#peopleModule').find('input').val());
-        $('#peopleModule').find('input').val('');
-        // data for Mustache template
-        var data = {
-            people: people
-        };
-        $('#peopleModule').find('ul').html(Mustache.render(template, data));
-    });
+            this.people.splice(i, 1);
+            this.render();
+        },
+        render() {
+            var data = {
+                people: this.people
+            };
+            this.$ul.html(Mustache.render(this.template, data));
+        },
+        init() {
+            this.cacheDom();
+            this.render();
+            this.bindEvents();
+        }
+    };
 
-    $('#peopleModule').find('ul').on('click', 'i.del', function(e) {
-        var $remove = $(e.target).closest('li');
-        var i = $('#peopleModule').find('ul').find('li').index($remove);
+    people.init();
 
-        $remove.remove();
+})();
 
-        people.splice(i, 1);
-    });
-});
+// $(function() {
+
+
+//     $('#peopleModule').find('button').on('click', function() {
+//         people.push($('#peopleModule').find('input').val());
+//         $('#peopleModule').find('input').val('');
+//         // data for Mustache template
+//         var data = {
+//             people: people
+//         };
+//         $('#peopleModule').find('ul').html(Mustache.render(template, data));
+//     });
+
+//     $('#peopleModule').find('ul').on('click', 'i.del', function(e) {
+//         var $remove = $(e.target).closest('li');
+//         var i = $('#peopleModule').find('ul').find('li').index($remove);
+
+//         $remove.remove();
+
+//         people.splice(i, 1);
+//     });
+// });
